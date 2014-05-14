608ce7cbb383023b8b7369aac2a1edac375a494a
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-14 20:16:03 +0800

    Update test cases

--- test/ReSpec.hs
+++ test/ReSpec.hs
@@ -6,13 +6,30 @@ import Re
 
 spec :: Spec
 spec = do
-    describe "test" $ do
-        context "basic regular express" $ do
-            it "nothing special" $ do
-                let pattern = "12345"
-                let string = "12345"
+    describe "Re" $ do
+        context "match" $ do
+            it "broken pattern" $ do
+                (match "++" "12345") `shouldBe` Nothing
+                (match "1++" "12345") `shouldBe` Nothing
 
-                (match pattern string) `shouldBe` Just True
+            it "no special charater pattern" $ do
+                (match "12345" "12345") `shouldBe` Just True
+                (match "12345" "1234") `shouldBe` Just False
+
+            it "*" $ do
+                (match "a*b" "b") `shouldBe` Just True
+                (match "a*b" "ab") `shouldBe` Just True
+                (match "a*b" "aab") `shouldBe` Just True
+
+            it "?" $ do
+                (match "a?b" "b") `shouldBe` Just True
+                (match "a?b" "ab") `shouldBe` Just True
+                (match "a?b" "aab") `shouldBe` Just False
+
+            it "+" $ do
+                (match "a+b" "b") `shouldBe` Just False
+                (match "a+b" "ab") `shouldBe` Just True
+                (match "a+b" "aab") `shouldBe` Just True
 
 main :: IO()
 main = do
