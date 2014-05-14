83af21335ffc61a853aa748d86312942bf6ffa80
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-14 21:23:06 +0800

    Simplify test cases

--- test/ReSpec.hs
+++ test/ReSpec.hs
@@ -6,30 +6,29 @@ import Re
 
 spec :: Spec
 spec = do
-    describe "Re" $ do
-        context "match" $ do
-            it "broken pattern" $ do
-                (match "++" "12345") `shouldBe` Nothing
-                (match "1++" "12345") `shouldBe` Nothing
+    describe "match" $ do
+        it "broken pattern" $ do
+            (match "++" "12345") `shouldBe` Nothing
+            (match "1++" "12345") `shouldBe` Nothing
 
-            it "no special charater pattern" $ do
-                (match "12345" "12345") `shouldBe` Just True
-                (match "12345" "1234") `shouldBe` Just False
+        it "no special charater pattern" $ do
+            (match "12345" "12345") `shouldBe` Just True
+            (match "12345" "1234") `shouldBe` Just False
 
-            it "*" $ do
-                (match "a*b" "b") `shouldBe` Just True
-                (match "a*b" "ab") `shouldBe` Just True
-                (match "a*b" "aab") `shouldBe` Just True
+        it "*" $ do
+            (match "a*b" "b") `shouldBe` Just True
+            (match "a*b" "ab") `shouldBe` Just True
+            (match "a*b" "aab") `shouldBe` Just True
 
-            it "?" $ do
-                (match "a?b" "b") `shouldBe` Just True
-                (match "a?b" "ab") `shouldBe` Just True
-                (match "a?b" "aab") `shouldBe` Just False
+        it "?" $ do
+            (match "a?b" "b") `shouldBe` Just True
+            (match "a?b" "ab") `shouldBe` Just True
+            (match "a?b" "aab") `shouldBe` Just False
 
-            it "+" $ do
-                (match "a+b" "b") `shouldBe` Just False
-                (match "a+b" "ab") `shouldBe` Just True
-                (match "a+b" "aab") `shouldBe` Just True
+        it "+" $ do
+            (match "a+b" "b") `shouldBe` Just False
+            (match "a+b" "ab") `shouldBe` Just True
+            (match "a+b" "aab") `shouldBe` Just True
 
 main :: IO()
 main = do
