73b2bed62133857b683ee7b3aecc0e40bb9ff7d0
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-14 19:58:41 +0800

    Add ReSpec.hs

---
+++ test/ReSpec.hs
@@ -0,0 +1,17 @@
+module ReSpec where
+
+import Re
+
+spec :: Spec
+spec = do
+    describe "test" $ do
+        context "basic regular express" $ do
+            it "nothing special" $ do
+                let pattern = "12345"
+                let string = "12345"
+
+                (match pattern string) `shouldBe` Just True
+
+main :: IO()
+main = do
+    hspec spec
